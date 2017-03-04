93d8a1684c45c79a2487495220948dec2f9e1119
Tona <carlostonatihu@gmail.com>
2017-03-04 16:23:58 -0600

    Ya esta corregido un error muy trivial

--- p2/mostar_copiar.c
+++ p2/mostar_copiar.c
@@ -17,6 +17,7 @@ int mostar_contenido(char *archivo){
 	int archivo_abierto = 0;
 	strtok(archivo, "\n");
 	ssize_t size = obtener_size(archivo, &archivo_abierto);
+	printf("Mostar: --%s-- size %d\n", archivo, size);
 	char buffer[size];
 	int i = 0;
 	strcpy(buffer, "");
@@ -33,7 +34,11 @@ int mostar_contenido(char *archivo){
 int copiar_archivo(char *archivo, char *nuevo_direcctorio){
 	int archivo_abierto = 0;
 	strtok(archivo, "\n");
+	strtok(nuevo_direcctorio, "\n");
+	
 	ssize_t size = obtener_size(archivo, &archivo_abierto);
+	printf("Copiar: --%s-- size %d\n", archivo, size);
+
 	char buffer[size];
 	strcpy(buffer, "");
 	read(archivo_abierto, buffer, size);
@@ -42,6 +47,8 @@ int copiar_archivo(char *archivo, char *nuevo_direcctorio){
 	chdir(nuevo_direcctorio);
 	int archivo_nuevo = open(archivo, O_RDONLY | O_WRONLY | O_TRUNC | O_CREAT, 0777);
 	write(archivo_nuevo, buffer, size);
+	close(archivo_nuevo);
+	chdir("..");
 	return 0;
 }
 
@@ -53,6 +60,8 @@ int main(int argc, char *argv[]) {
 	int c;
 	printf("Mostar contenido de un archivo o copiar archivos\n");
 	while(continuar == 's'){
+		strcpy(archivo, "");
+		strcpy(directorio, "");
 		printf("%s\n", "Qué quieres hacer?");
 		printf("%s\n", "Mostar contenido de un archivo........(1)");
 		printf("%s\n", "Copiar archivos.......................(2)");
