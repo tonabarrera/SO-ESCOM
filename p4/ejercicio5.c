f24d66ce30ac1dfcedd3f173f5f2552b57f19ad0
Tonatihu <carlostonatihu@gmail.com>
2017-04-04 23:11:33 -0500

    Suma y resta

---
+++ p4/ejercicio5.c
@@ -0,0 +1,65 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+#define col 10
+
+int suma(int matrizA[col][col], int matrizB[col][col], int resultado[col][col]) {
+    int i, j;
+    for (i = 0; i < col; i++) {
+        for (j = 0; j < col; j++) {
+            resultado[i][j] = matrizA[i][j] + matrizB[i][j];
+        }
+    }
+    return 0;
+}
+
+
+int resta(int matrizA[col][col], int matrizB[col][col], int resultado[col][col]) {
+    int i, j;
+    for (i = 0; i < col; i++) {
+        for (j = 0; j < col; j++) {
+            resultado[i][j] = matrizA[i][j] - matrizB[i][j];
+        }
+    }
+    return 0;
+}
+
+
+int main(int argc, char* argv[]){
+    int matrizSuma[col][col];
+    int matrizResta[col][col];/*
+    int matrizProducto[col][col];
+    int matrizTraspuesta[col][col];
+    int matrizInversa[col][col];*/
+    int matrizA[col][col] = {
+        {1, 2, 3, 4, 5, 6, 7, 8, 9, 10},
+        {1, 2, 3, 4, 5, 6, 7, 8, 9, 10},
+        {1, 2, 3, 4, 5, 6, 7, 8, 9, 10},
+        {1, 2, 3, 4, 5, 75, 7, 8, 9, 10},
+        {1, 2, 3, 4, 5, 6, 7, 8, 9, 10},
+        {1, 2, 3, 4, 5, 6, 7, 8, 9, 10},
+        {1, 2, 3, 4, 5, 6, 7, 8, 9, 10},
+        {1, 2, 3, 4, 5, 6, 7, 8, 9, 10},
+        {1, 2, 3, 4, 5, 6, 7, 8, 9, 10},
+        {1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
+    };
+    int matrizB[col][col] = {
+        {1, 2, 3, 4, 5, 6, 7, 8, 9, 10},
+        {1, 2, 3, 4, 5, 6, 7, 8, 9, 10},
+        {1, 2, 3, 4, 5, 6, 7, 8, 9, 10},
+        {1, 2, 3, 4, 5, 6, 7, 8, 9, 10},
+        {1, 2, 3, 4, 5, 6, 7, 8, 9, 10},
+        {1, 2, 3, 4, 5, 6, 7, 8, 9, 10},
+        {1, 2, 3, 4, 5, 6, 7, 8, 9, 10},
+        {1, 2, 3, 4, 5, 6, 7, 8, 9, 10},
+        {1, 2, 3, 4, 5, 6, 7, 8, 9, 10},
+        {1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
+    };
+    suma(matrizA, matrizB, matrizSuma);
+    resta(matrizA, matrizB, matrizResta);
+    printf("Valor %d\n", matrizSuma[9][9]);
+    printf("Valor %d\n", matrizResta[3][5]);
+    return 0;
+}
+
