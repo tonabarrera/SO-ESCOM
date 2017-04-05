ba05abaefb835dc8d8b6aec53d051de5d59c5a48
Tonatihu <carlostonatihu@gmail.com>
2017-04-04 22:00:57 -0500

    Harcodeado pero ya esta la cuatro

--- p4/ejercicio4.c
+++ p4/ejercicio4.c
@@ -3,108 +3,196 @@
 #include <stdlib.h>
 #include <sys/wait.h>
 
+void prueba(int i, int pid){
+    printf("Primero-%d %d\n", i, getpid());
+    pid = fork();
+    if (pid == 0) {
+        printf("Segundo-%d %d\n", i, getpid());
+        pid = fork();
+        if (pid == 0) {
+            printf("Tercero-%d %d\n\n", i, getppid());
+            exit(0);
+        } else
+            wait(NULL);
+        exit(0);
+    } else
+        wait(NULL);
+    exit(0);
+}
+
+void prueba2(int i, int pid) {
+    printf("Primero-%d %d\n", i, getpid());
+    pid = fork();
+    if (pid == 0) {
+    printf("Segundo-%d %d\n", i, getpid());
+    pid = fork();
+    if (pid == 0) {
+        printf("Tercero-%d %d\n", i, getpid());
+        int j;
+        for(j=0; j<2; j++){
+            pid = fork();
+            if(pid == 0){
+                printf("Cuarto-%d-%d %d\n", i, j, getppid());
+                exit(0);
+            } else
+                wait(NULL);
+        }
+        printf("\n");
+        exit(0);
+    } else
+        wait(NULL);
+    exit(0);
+    } else
+        wait(NULL);
+    exit(0);
+}
+
+void prueba3(int i, int pid){
+    printf("Primero-%d %d\n", i, getpid());
+    pid = fork();
+    if (pid == 0) {
+        printf("Segundo-%d %d\n", i, getpid());
+        pid = fork();
+        if (pid == 0) {
+            printf("Tercero-%d %d\n", i, getpid());
+            pid = fork();
+            if (pid == 0) {
+                printf("Cuarto-%d %d\n", i, getpid());
+                 int j;
+                for(j=0; j<3; j++){
+                    pid = fork();
+                    if(pid == 0){
+                        printf("Quinto-%d-%d %d\n", i, j, getppid());
+                        exit(0);
+                    } else
+                        wait(NULL);
+                }
+                printf("\n");
+                exit(0);
+            } else
+                wait(NULL);
+            exit(0);
+        } else
+            wait(NULL);
+        exit(0);
+    } else
+        wait(NULL);
+    exit(0);
+}
+
+void prueba4(int i, int pid){
+    printf("Primero-%d %d\n", i, getpid());
+    pid = fork();
+    if (pid == 0) {
+        printf("Segundo-%d %d\n", i, getpid());
+        pid = fork();
+        if (pid == 0) {
+            printf("Tercero-%d %d\n", i, getpid());
+            pid = fork();
+            if (pid == 0) {
+                printf("Cuarto-%d %d\n", i, getpid());
+                pid = fork();
+                if (pid == 0){
+                    printf("Quinto-%d %d\n", i, getpid());
+                    int j;
+                    for(j=0; j<4; j++){
+                        pid = fork();
+                        if(pid == 0){
+                            printf("Sexto-%d-%d %d\n", i, j, getppid());
+                            exit(0);
+                        } else
+                            wait(NULL);
+                    }
+                    printf("\n");
+                    exit(0);
+                } else
+                    wait(NULL);
+                exit(0);
+            } else
+                wait(NULL);
+            exit(0);
+        } else
+            wait(NULL);
+        exit(0);
+    } else
+        wait(NULL);
+    exit(0);
+}
+
+void prueba5(int i, int pid){
+    printf("Primero-%d %d\n", i, getpid());
+    pid = fork();
+    if (pid == 0) {
+        printf("Segundo-%d %d\n", i, getpid());
+        pid = fork();
+        if (pid == 0) {
+            printf("Tercero-%d %d\n", i, getpid());
+            pid = fork();
+            if (pid == 0) {
+                printf("Cuarto-%d %d\n", i, getpid());
+                pid = fork();
+                if (pid == 0){
+                    printf("Quinto-%d %d\n", i, getpid());
+                    pid = fork();
+                    if (pid == 0){
+                        printf("Sexto-%d %d\n", i, getpid());
+                        int j;
+                        for(j=0; j<5; j++){
+                            pid = fork();
+                            if(pid == 0){
+                                printf("Septimo-%d-%d %d\n", i, j, getppid());
+                                exit(0);
+                            } else
+                                wait(NULL);
+                        }
+                        printf("\n");
+                        exit(0);
+                    } else
+                        wait(NULL);
+                    exit(0);
+                } else
+                    wait(NULL);
+                exit(0);
+            } else
+                wait(NULL);
+            exit(0);
+        } else
+            wait(NULL);
+        exit(0);
+    } else
+        wait(NULL);
+    exit(0);
+}
+
 int main(int argc, char* argv[]) {
     int i;
     int pid;
-    printf("Raiz: %d\n", getpid());
-    for (i = 0; i < 7; i++) {
+    printf("Raiz: %d\n\n", getpid());
+    for (i = 0; i < 10; i++) {
         pid = fork();
-        if (pid==0 && i != 4 && i!=5 && i !=6 && i!=3) {
-            printf("Primero-%d %d\n", i, getpid());
-            exit(0);
-        }else {
-            wait(NULL);
-        }
-        if (pid==0 && i==3) {
-            printf("Primero-%d %d\n", i, getpid());
-            pid = fork();
-            if (pid==0) {
-                printf("Segundo-%d %d\n", i, getpid());
-                pid = fork();
-                if (pid==0) {
-                    printf("Tercero-%d %d\n", i, getpid());
-                    int j;
-                    for(j=0; j<2; j++){
-                        pid = fork();
-                        if(pid==0){
-                            printf("Cuarto-%d-%d %d\n", i, j, getppid());
-                            exit(0);
-                        } else {
-                            wait(NULL);
-                        }
-                    }
-                    exit(0);
-                } else {
-                    wait(NULL);
-                }
-                exit(0);
-            } else {
-                wait(NULL);
-            }
-            exit(0);
-        } else if (pid==0 && i==4) {
-            printf("Primero-%d %d\n", i, getpid());
-            pid = fork();
-            if(pid==0) {
-                printf("Segundo-%d %d\n", i, getpid());
-                pid = fork();
-                if (pid==0) {
-                    printf("Tercero-%d %d\n", i, getppid());
-                    exit(0);
-                } else {
-                    wait(NULL);
-                }
-                exit(0);
-            }else {
-                wait(NULL);
-            }
-            exit(0);
-        } else if(pid==0 && i==5){
-            printf("Primero-%d %d\n", i, getpid());
-            pid = fork();
-            if (pid==0) {
-                printf("Segundo-%d %d\n", i, getpid());
-                pid = fork();
-                if (pid==0) {
-                    printf("Tercero-%d %d\n", i, getppid());
-                    exit(0);
-                } else {
-                    wait(NULL);
-                }
-                exit(0);
-            } else {
-                wait(NULL);
-            }
-            exit(0);
-        } else if (pid==0 && i==6) {
-            printf("Primero-%d %d\n", i, getpid());
-            pid = fork();
-            if (pid==0) {
-                printf("Segundo-%d %d\n", i, getpid());
-                pid = fork();
-                if (pid==0) {
-                    printf("Tercero-%d %d\n", i, getpid());
-                    int j;
-                    for(j=0; j<2; j++){
-                        pid = fork();
-                        if(pid==0){
-                            printf("Cuarto-%d-%d %d\n", i, j, getppid());
-                            exit(0);
-                        } else {
-                            wait(NULL);
-                        }
-                    }
-                    exit(0);
-                } else {
-                    wait(NULL);
-                }
-                exit(0);
-            } else {
-                wait(NULL);
-            }
-            exit(0);
-        } else
+        if (pid==0 && i==0)
+            prueba5(i, pid);
+        else if (pid==0 && i==1)
+            prueba4(i, pid);
+        else if (pid==0 && i==2)
+            prueba3(i, pid);
+        else if (pid==0 && i==3)
+            prueba2(i, pid);
+        else if (pid==0 && i==4)
+            prueba(i, pid);
+        else if(pid==0 && i==5)
+            prueba(i, pid);
+        else if (pid==0 && i==6)
+            prueba2(i, pid);
+        else if (pid==0 && i==7)
+            prueba3(i, pid);
+        else if (pid==0 && i==8)
+            prueba4(i, pid);
+        else if (pid==0 && i==9)
+            prueba5(i, pid);
+        else
             wait(NULL);
     }
     return 0;
 }
+
