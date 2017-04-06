934aef5eebc69db6cd856517e31f420360e3e16e
Carlos Barrera <carlostonatihu@gmail.com>
2017-04-06 12:56:25 -0500

    Ya esta el ejemplo

---
+++ p4/windows1.c
@@ -0,0 +1,25 @@
+#include <windows.h>
+#include <stdio.h>
+
+int main(int argc, char *argv[]) {
+    STARTUPINFO si;
+    PROCESS_INFORMATION pi;
+    ZeroMemory(&si, sizeof(si));
+    si.cb = sizeof(si);
+    ZeroMemory(&pi, sizeof(pi));
+    if (argc!=2) {
+        printf("Usar %s windows2.c\n", argv[0]);
+        return 0;
+    }
+
+    if (!CreateProcess(NULL, argv[1], NULL, NULL, FALSE, 0, NULL, NULL, &si, &pi)) {
+        printf("Fallo al invocar CreateProcess (%d)\n", (int)GetLastError());
+        return 0;
+    }
+    printf("Soy el padre\n");
+    WaitForSingleObject(pi.hProcess, INFINITE);
+
+    CloseHandle(pi.hProcess);
+    CloseHandle(pi.hThread);
+    return 0;
+}
---
+++ p4/windows8.c
@@ -0,0 +1,92 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/wait.h>
+#include <unistd.h>
+#include "funciones5.h"
+
+int main(int argc, char* argv[]){
+    int pid;
+    int matrizSuma[col][col];
+    int matrizResta[col][col];
+    int producto[col][col];
+    int traspuestaA[col][col];
+    int traspuestaB[col][col];
+    //int matrizInversa[col][col];
+    int matrizA[col][col] = {
+        {1, 2, 3, 4, 5, 6, 7, 8, 9, 10},
+        {1, 2, 3, 4, 5, 6, 7, 8, 9, 10},
+        {1, 2, 3, 4, 5, 6, 7, 8, 9, 10},
+        {1, 2, 3, 4, 5, 6, 7, 8, 9, 10},
+        {1, 2, 3, 4, 5, 6, 7, 8, 9, 10},
+        {1, 2, 3, 4, 5, 6, 7, 8, 9, 10},
+        {1, 2, 3, 4, 5, 6, 7, 8, 9, 10},
+        {1, 2, 3, 4, 5, 6, 7, 8, 9, 10},
+        {1, 2, 3, 4, 5, 6, 7, 8, 9, 10},
+        {1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
+    };
+    int matrizB[col][col] = {
+        {1, 2, 3, 4, 5, 6, 7, 8, 9, 10},
+        {1, 2, 3, 4, 5, 6, 7, 8, 9, 10},
+        {1, 2, 3, 4, 5, 6, 7, 8, 9, 10},
+        {1, 2, 3, 4, 5, 6, 7, 8, 9, 10},
+        {1, 2, 3, 4, 5, 6, 7, 8, 9, 10},
+        {1, 2, 3, 4, 5, 6, 7, 8, 9, 10},
+        {1, 2, 3, 4, 5, 6, 7, 8, 9, 10},
+        {1, 2, 3, 4, 5, 6, 7, 8, 9, 10},
+        {1, 2, 3, 4, 5, 6, 7, 8, 9, 10},
+        {1, 2, 3, 4, 5, 6, 7, 8, 9, 100}
+    };
+    pid = fork();
+    if (pid == 0) {
+        int i;
+        for(i=0; i<4; i++){
+            pid = fork();
+            if(pid==0 && i==0){
+                printf("%s\n", "Suma");
+                sumar(matrizA, matrizB, matrizSuma);
+                int ficheroSuma = abrir("suma.txt");
+                escribir(ficheroSuma, matrizSuma);
+                close(ficheroSuma);
+                exit(0);
+            } else if (pid==0 && i==1){
+                printf("%s\n", "Resta");
+                restar(matrizA, matrizB, matrizResta);
+                int ficheroResta = abrir("resta.txt");
+                escribir(ficheroResta, matrizResta);
+                close(ficheroResta);
+                exit(0);
+            } else if (pid==0 && i==2){
+                printf("%s\n", "Multiplicacion");
+                multiplicar(matrizA, matrizB, producto);
+                int ficheroMul = abrir("multiplicacion.txt");
+                escribir(ficheroMul, producto);
+                close(ficheroMul);
+                exit(0);
+            } else if (pid==0 && i==3){
+                printf("%s\n", "Transpuesta");
+                trasponer(matrizA, traspuestaA);
+                trasponer(matrizB, traspuestaB);
+                int ficheroTras = abrir("traspuesta.txt");
+                escribir(ficheroTras, traspuestaA);
+                escribir(ficheroTras, traspuestaB);
+                close(ficheroTras);
+                exit(0);
+            } else
+                wait(NULL);
+        }
+        exit(0);
+    } else {
+        wait(NULL);
+        printf("%s\n", "Salida");
+        printf("%s\n", "Suma de matrices");
+        imprimir("suma.txt");
+        printf("%s\n", "Resta de matrices");
+        imprimir("resta.txt");
+        printf("%s\n", "Multiplicacion de matrices");
+        imprimir("multiplicacion.txt");
+        printf("%s\n", "Traspuesta de las dos matrices");
+        imprimir("traspuesta.txt");
+        printf("%s\n", "Iversa de matrices");
+    }
+    return 0;
+}
