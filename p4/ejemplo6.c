ca8989382feb03cff889c898932f0b54c3d05eb0
Tonatihu <carlostonatihu@gmail.com>
2017-04-05 23:12:22 -0500

    Ejemplo

---
+++ p4/ejemplo6.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <stdlib.h>
+int main() {
+    pid_t pid;
+    char *argv[3];
+    argv[0] = "./hola";
+    argv[1] = "Desde el hijo";
+    argv[2] = NULL;
+    if ((pid=fork())==0) {
+        printf("Soy el hijo ejecutando: %s\n", argv[0]);
+        execv(argv[0], argv);
+    } else {
+        wait(NULL);
+        printf("Soy el padre\n");
+        exit(0);
+    }
+    return 0;
+}
