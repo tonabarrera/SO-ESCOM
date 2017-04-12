9cc07531c503e47172a81895050ea349bbf95fb2
Carlos Barrera <carlostonatihu@gmail.com>
2017-04-12 18:03:30 -0500

    Lindo :poop:

--- p4/funciones5.c
+++ p4/funciones5.c
@@ -1,5 +1,5 @@
 #include "funciones5.h"
-#define SWAP(a, b) {temp=(a);(a)=(b);(b)=temp;}
+#define SWAP(a, b) {temp = (a);(a) = (b);(b) = temp;}
 
 int sumar(int matrizA[COL][COL], int matrizB[COL][COL], int resultado[COL][COL]) {
     int i, j;
@@ -21,8 +21,8 @@ int restar(int matrizA[COL][COL], int matrizB[COL][COL], int resultado[COL][COL]
 
 int trasponer(int matriz[COL][COL], int traspuesta[COL][COL]) {
     int i, j;
-    for(i=0;i<COL; i++){
-        for(j=0; j<COL; j++)
+    for(i = 0;i < COL; i++){
+        for(j = 0; j < COL; j++)
             traspuesta[j][i] = matriz[i][j];
     }
     return 0;
@@ -31,11 +31,11 @@ int trasponer(int matriz[COL][COL], int traspuesta[COL][COL]) {
 int multiplicar(int matrizA[COL][COL], int matrizB[COL][COL], int resultado[COL][COL]) {
     int i, j, k;
     int valor = 0;
-    for(i=0; i<COL; i++){
-        for(j=0; j<COL; j++){
+    for(i = 0; i < COL; i++){
+        for(j = 0; j < COL; j++){
             valor = 0;
-            for(k=0; k<COL; k++){
-                valor+= matrizA[i][k]*matrizB[k][j];
+            for(k = 0; k < COL; k++){
+                valor += matrizA[i][k] * matrizB[k][j];
             }
             resultado[i][j] = valor;
         }
@@ -53,7 +53,7 @@ float **matriz() {
         exit(1);
     }
 
-    for(i=0; i<COL; i++)
+    for(i = 0; i < COL; i++)
         m[i] = (float *) malloc((size_t)((COL)*sizeof(float)));
 
     if(!m[COL-1]){
@@ -73,71 +73,70 @@ int copiar_matriz(float **destino, int origen[COL][COL]){
 }
 
 int gaussj(float **inversa) {
-    int *indxc, *indxr, *ipiv;
-    int i, icol, irow, j, k, l, ll;
-    float big, dum, pivinv, temp;
+    int *columnas, *filas, *pivotes;
+    int i, icol, ifila, j, k, l, ll;
+    float mayor, residuo, pivinv, temp;
 
-    indxc = (int *) malloc((size_t)((COL)*sizeof(int)));;
-    indxr = (int *) malloc((size_t)((COL)*sizeof(int)));
-    ipiv = (int *) malloc((size_t)((COL)*sizeof(int)));
+    columnas = (int *) malloc((size_t)((COL)*sizeof(int)));;
+    filas = (int *) malloc((size_t)((COL)*sizeof(int)));
+    pivotes = (int *) malloc((size_t)((COL)*sizeof(int)));
 
-    for (j=0; j < COL; j++)
-        ipiv[j]=0;
-
-    for (i=0; i < COL; i++){
-        big = 0.0;
-        for (j=0; j < COL; j++){
-            if(ipiv[j] !=1){
-                for (k=0; k<COL; k++){
-                    if(ipiv[k] == 0){
-                        if(fabs(inversa[j][k]) >= big){
-                            big = fabs(inversa[j][k]);
-                            irow = j;//2 2
+    for (j = 0; j < COL; j++)
+        pivotes[j]=0;
+    for (i = 0; i < COL; i++){
+        mayor = 0.0;
+        for (j = 0; j < COL; j++){
+            if(pivotes[j] != 1){
+                for (k=0; k < COL; k++){
+                    if(pivotes[k] == 0){
+                        if(fabs(inversa[j][k]) >= mayor){
+                            mayor = fabs(inversa[j][k]);
+                            ifila = j;//2 2
                             icol = k;//1 2
                         }
-                    } else if(ipiv[k]>1) {
+                    } else if(pivotes[k] > 1) {
                         printf("---Error, la matriz es no invertible--\n");
                         return -1;
                     }
                 }
             }
         }
-        ++(ipiv[icol]);//ipiv[1]=1 ipiv[2]=2
-        if (irow != icol) {
-            for (l=0; l<COL; l++) SWAP(inversa[irow][l], inversa[icol][l]);
+        ++(pivotes[icol]);//pivotes[1]=1 pivotes[2]=2
+        if (ifila != icol) {
+            for (l = 0; l < COL; l++) SWAP(inversa[ifila][l], inversa[icol][l]);
         }
-        indxr[i] = irow;//2 2
-        indxc[i] = icol;//1 2
-        if (inversa[icol][icol]==0.0) {
+        filas[i] = ifila;//2 2
+        columnas[i] = icol;//1 2
+        if (inversa[icol][icol] == 0.0) {
             printf("--Error, la matriz es no invertible--\n");
             return -1;
         }
-        pivinv = 1.0/inversa[icol][icol]; //dividir fila del pivote
+        pivinv = 1.0/inversa[icol][icol]; //dividir fila del pivotes
         inversa[icol][icol] = 1.0;
 
         //Continnuar la division de columnas de la fila en la que nos encontramos
-        for (l=0; l < COL; l++)
+        for (l = 0; l < COL; l++)
             inversa[icol][l] *= pivinv;
-        //resta menos en la fila del pivote icol
-        for (ll=0; ll < COL; ll++){
+        //resta menos en la fila del pivotes icol
+        for (ll = 0; ll < COL; ll++){
             if(ll != icol) {
-                dum = inversa[ll][icol];
-                inversa[ll][icol]=0.0;
-                for (l=0; l < COL; l++)
-                    inversa[ll][l] -= inversa[icol][l]*dum;
+                residuo = inversa[ll][icol];
+                inversa[ll][icol] = 0.0;
+                for (l = 0; l < COL; l++)
+                    inversa[ll][l] -= inversa[icol][l] * residuo;
             }
         }
     }
 
-    for (l=COL-1; l >= 0; l--){
-        if(indxr[l] != indxc[l]){
-            for(k=0; k<COL; k++)
-                SWAP(inversa[k][indxr[l]], inversa[k][indxc[l]]);
+    for (l = COL-1; l >= 0; l--){
+        if(filas[l] != columnas[l]){
+            for(k = 0; k < COL; k++)
+                SWAP(inversa[k][filas[l]], inversa[k][columnas[l]]);
         }
     }
-    free(indxr);
-    free(indxc);
-    free(ipiv);
+    free(filas);
+    free(columnas);
+    free(pivotes);
     return 0;
 }
 
@@ -156,8 +155,8 @@ int escribir_matriz(int fichero, int matriz[COL][COL]) {
     int m, n;
     char caracter[5];
     write(fichero, "[\n", 2);
-    for (m=0; m<COL; m++){
-        for(n=0; n<COL; n++){
+    for (m = 0; m < COL; m++){
+        for(n = 0; n < COL; n++){
             sprintf(caracter, "%d", matriz[m][n]);
             write(fichero, caracter, strlen(caracter));
             write(fichero, " ", 1);
@@ -173,8 +172,8 @@ int escribir_float(int fichero, float **matriz) {
     int m, n;
     char caracter[5];
     write(fichero, "[\n", 2);
-    for (m=0; m<COL; m++){
-        for(n=0; n<COL; n++){
+    for (m = 0; m < COL; m++){
+        for(n = 0; n < COL; n++){
             sprintf(caracter, "%.2f", matriz[m][n]);
             write(fichero, caracter, strlen(caracter));
             write(fichero, " ", 1);
@@ -199,8 +198,8 @@ int imprimir(char *archivo) {
 int mostrar_matriz(int matriz[COL][COL]) {
     int i, j;
     printf("[\n");
-    for (i=0; i<COL; i++){
-        for(j=0; j<COL; j++)
+    for (i = 0; i < COL; i++){
+        for(j = 0; j < COL; j++)
             printf("%d ", matriz[i][j]);
         printf("\n");
     }
