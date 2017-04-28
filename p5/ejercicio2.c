af45fe102b342eec347fd9485591da3dfe6790ac
Tonatihu <carlostonatihu@gmail.com>
2017-04-28 18:11:12 -0500

    Solucionando el error mas pendejo que he hecho

--- p5/ejercicio2.c
+++ p5/ejercicio2.c
@@ -8,26 +8,24 @@
 #include <fcntl.h>
 #include <unistd.h>
 struct nodo {
-    pthread_t hilo;
+    pthread_t *hilo;
     struct nodo *next;
 };
 struct contenedor {
     char *origen;
     char *destino;
     char *ruta;
-    struct nodo *next;
 };
 void *lectura_archivos(void *arg);
 
 int main(int argc, char const* argv[]) {
     struct contenedor *cont = malloc(sizeof(struct contenedor));
-    char *origen = "prueba/";
-    cont->origen = origen;
-    cont->ruta="";
+    cont->origen = "prueba/";
+    cont->ruta = "";
     cont->destino = "dest/";
-    pthread_t hilo;
-    pthread_create(&hilo, NULL, lectura_archivos, (void*)cont);
-    pthread_join(hilo, NULL);
+    pthread_t *hilo = malloc(sizeof(pthread_t));
+    pthread_create(hilo, NULL, lectura_archivos, (void*)cont);
+    pthread_join(*hilo, NULL);
     return 0;
 }
 
@@ -39,34 +37,56 @@ void *lectura_archivos(void *arg) {
     char nana[100];
     char *origen = super->origen;
     strcpy(nana, origen);
+    //printf("Primera comprobacion %s\n", super->ruta);
     strcat(nana, super->ruta);
+    char el_path[100];
+    strcpy(el_path, super->ruta);
     directorio = opendir(nana);
-    printf("Nana es %s Origen es %s\n", nana, origen);
+    printf("Path es %s Origen es %s\n", nana, origen);
+    struct nodo *elemento = NULL;
     int i = 0;
     while ((archivo = readdir(directorio)) != NULL) {
         if (strcmp(archivo->d_name, ".") && strcmp(archivo->d_name, "..")) {
             strcpy(nana, origen);
-            strcat(nana, super->ruta);
+            strcat(nana, el_path);
             if (stat(strcat(nana, archivo->d_name), &datos_archivo) == 0){
                 printf("El archivo es %s ", archivo->d_name);
                 if (S_ISDIR(datos_archivo.st_mode)){
                     printf("Es un directorio - ");
-                    pthread_t hilo;
+                    pthread_t *hilo = malloc(sizeof(pthread_t));
                     char nuevo[100];
                     char copia[100];
-                    strcpy(nuevo, super->ruta);
+                    strcpy(nuevo, el_path);
                     strcat(nuevo, archivo->d_name);
                     strcat(nuevo, "/");
                     struct contenedor *siguiente = malloc(sizeof(struct contenedor));
                     siguiente->origen = super->origen;
-                    siguiente->ruta= nuevo;
+                    siguiente->ruta = malloc(sizeof(nuevo));
+                    strcpy(siguiente->ruta, nuevo);
                     siguiente->destino = super->destino;
                     printf("Proximo direcctorio %s\n", nuevo);
                     strcpy(copia, super->destino);
                     strcat(copia, nuevo);
                     mkdir(copia, 0777);
-                    pthread_create(&hilo, NULL, lectura_archivos, (void*)siguiente);
-                    pthread_join(hilo, NULL);
+                    struct nodo *temp = malloc(sizeof(struct nodo));
+                    temp->next = NULL;
+                    temp->hilo = hilo;
+                    //printf("El id del hilo %p %p\n", hilo, temp);
+                    if (elemento == NULL) {
+                        elemento = temp;
+                        //printf("Es null %s\n", archivo->d_name);
+                    } else {
+                        //printf("No es null %s\n", archivo->d_name);
+                        struct nodo *tonto = elemento;
+                        while (tonto->next != NULL) {
+                            tonto = tonto->next;
+                        }
+                        //printf("Debe de ser null %p\n", elemento);
+                        tonto->next = temp;
+                    }
+                    //printf("El apuntador es %p %p\n",siguiente, siguiente->ruta);
+                    //printf("Ultima comprobacion %s\n", siguiente->ruta);
+                    pthread_create(hilo, NULL, lectura_archivos, (void*)siguiente);
                 } else {
                     //copiar archivos
                     char super_archivo[100];
@@ -78,7 +98,7 @@ void *lectura_archivos(void *arg) {
                     strcat(ruta_vieja, super->ruta);
                     strcat(super_archivo, archivo->d_name);
                     strcat(ruta_vieja, archivo->d_name);
-                    printf("La ruta para copiar es %s y la ruta vieja es: %s\n", super_archivo, ruta_vieja);
+                    //printf("La ruta para copiar es %s y la ruta vieja es: %s\n", super_archivo, ruta_vieja);
                     int archivo_abierto = open(ruta_vieja, O_RDONLY);
                     int archivo_clon = open(super_archivo, O_WRONLY | O_CREAT, 0640);
                     while (0 < read(archivo_abierto, buffer, 1))
@@ -92,5 +112,14 @@ void *lectura_archivos(void *arg) {
         }
         i++;
     }
-    return 0;
+    int valor = 0;
+    struct nodo *aux = elemento;
+    while (aux != NULL) {
+        //printf("---Duerme %i %s %p\n", i, super->ruta, aux);
+        pthread_join(*(aux->hilo), NULL);
+        aux = aux->next;
+        valor++;
+    }
+    //printf("el valor es %i\n", valor);
+    return NULL;
 }
