1e26ab7754a0f4a98691083e8ed05a387a05fbbb
Tonatihu <carlostonatihu@gmail.com>
2017-05-23 17:37:38 -0500

    Borrando la basura

--- p6/nieto.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <unistd.h>
-
-int main(int argc, char const* argv[]) {
-    printf("Nieto %i %i\n", getpid(), getppid());
-    exit(0);
-    return 0;
-}
